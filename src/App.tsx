c6645ef1bd77141a634a139001daccd31d4a4c03
Vitor Santos <pireshugo737@gmail.com>
2023-08-07 18:34:44 -0300

    :tada: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Let the threads of life get connected with yoga</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ stitches.config.ts
@@ -0,0 +1,42 @@
+import { createStitches, globalCss } from '@stitches/react';
+
+export const stitches = createStitches({
+  theme: {
+    fonts: {
+      poppins: 'Poppins, sans-serif',
+      merriweather: 'Merriweather Sans, sans-serif',
+    },
+
+    colors: {
+      darkGreen: '#374600 ',
+      titles: '#000000 ',
+      descriptions: '#575757 ',
+    },
+
+    fontWeights: {
+      lgt: 300,
+      reg: 400,
+      med: 500,
+      bd: 700,
+    },
+  },
+});
+
+globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  ul: { listStyleType: 'none' },
+
+  '.flex': { display: 'flex' },
+
+  'a, button': {
+    fontFamily: 'inherit',
+    color: 'inherit',
+  },
+
+  a: { textDecoration: 'none' },
+});
